58e454fbcb161903ae81ab2d9ca2cbfcca87686d
Fernando Tavares <73204469+nandoti@users.noreply.github.com>
2022-09-14 13:39:37 -0300

    feat: novas funcionalidades incluídas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Actions from "../components/Actions";
 import Balance from "../components/Balance";
 import Header from "../components/Header/index";
+import Movements from "../components/Movements";
+
+const list = [
+  {
+    id: 1,
+    label: "Salário",
+    value: "2.668,00",
+    date: "03/09/2022",
+    type: 1, // receita
+  },
+  {
+    id: 2,
+    label: "PIX",
+    value: "1.000,00",
+    date: "10/09/2022",
+    type: 1, // receita
+  },
+  {
+    id: 3,
+    label: "Boleto conta luz",
+    value: "129,00",
+    date: "11/09/2022",
+    type: 0, // despesas
+  },
+  {
+    id: 4,
+    label: "Cartão de crédito",
+    value: "800,00",
+    date: "12/09/2022",
+    type: 0, // despesas
+  },
+];
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="Fernando Tavares" />
+
       <Balance saldo="14.290,90" gastos="-900,00" />
+
+      <Actions />
+
+      <Text style={styles.title}>Últimas Movimentações</Text>
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String(item.id)}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <Movements data={item} />}
+      />
     </View>
   );
 }
@@ -14,5 +59,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fafafa",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    margin: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
   },
 });
